2d2294d02e9c837f1ee96b52c574643fd6d077a7
ThGnommy <th.brandoli@gmail.com>
2022-04-09 22:45:03 +0200

    add tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import TagManager, { TagManagerArgs } from "react-gtm-module";
+import { useEffect } from "react";
+
+const tagManagerArgs = {
+  id: "G-SMPR3T9YNZ",
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs as any);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
