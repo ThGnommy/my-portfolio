aedb3e2fed14763fb045a247d033523ddcc848b7
ThGnommy <th.brandoli@gmail.com>
2022-04-14 11:28:04 +0200

    create mini-game component

---
+++ components/MiniGame/MiniGame.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const MiniGame = () => {
+  return (
+    <motion.section>
+      <motion.div>Play</motion.div>
+      <motion.div>The Game</motion.div>
+    </motion.section>
+  );
+};
