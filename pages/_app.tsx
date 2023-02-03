36274249ae0979837437f4153459e073c7b002fb
ThGnommy <th.brandoli@gmail.com>
2023-02-03 17:01:11 +0100

    remove gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,12 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { useRouter } from "next/router";
 import { AnimatePresence, domAnimation, LazyMotion } from "framer-motion";
-import { useEffect } from "react";
-import TagManager from "react-gtm-module";
 
 const tagManagerArgs = {
   id: "GTM-K5R3KPC",
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs as any);
-  }, []);
-
   const router = useRouter();
   return (
     <Layout>
